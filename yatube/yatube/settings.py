18fd2b292183c020f016df1ef3f7f1d90f5f8f7a
Yuri Filatov <fyurikon@gmail.com>
2023-03-09 14:05:06 +0300

    5th sprint remove 3 blank lines from settings

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = [
     'testserver',
 ]
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -123,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
